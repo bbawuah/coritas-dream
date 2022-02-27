4ecfb72f52c3649e1c31113c17f12ce3fbb03718
bbawuah <brianbawuah@icloud.com>
2022-02-27 18:01:39 +0100

    remove three-mesh-bvh

--- components/webgl/floor/floor.tsx
+++ components/webgl/floor/floor.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
-import { useHelper } from '@react-three/drei';
-import { MeshBVH, MeshBVHVisualizer } from 'three-mesh-bvh';
 import { useStore } from '../../../store/store';
 
 interface Props {}
@@ -9,25 +7,7 @@ interface Props {}
 // When loading a scene, you should consider merging the geometry before creating MeshBVH instance
 export const Floor: React.FC<Props> = (props) => {
   const ref = useRef<THREE.Mesh>();
-  const { set } = useStore(({ set }) => ({ set }));
-
-  useEffect(() => {
-    if (ref.current) {
-      ref.current.geometry.boundsTree = new MeshBVH(ref.current.geometry);
-
-      set((state) => {
-        if (ref.current) {
-          // (ref.current.material as THREE.MeshStandardMaterial).wireframe = true;
-          (ref.current.material as THREE.MeshStandardMaterial).opacity = 0.2;
-
-          return { ...state, collider: ref.current };
-        }
-      });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  useHelper(ref, MeshBVHVisualizer);
+  // const { set } = useStore(({ set }) => ({ set }));
 
   return (
     <mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
