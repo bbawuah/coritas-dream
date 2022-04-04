62656b0dd79422fb57ba10491f91c85f322a07d3
bbawuah <brianbawuah@icloud.com>
2022-04-04 19:57:51 +0200

    feature(floor material): create shader material for floor

---
+++ components/webgl/floor/floorMaterial.ts
@@ -0,0 +1,40 @@
+import * as THREE from 'three';
+
+class FloorMaterial extends THREE.RawShaderMaterial {
+  constructor() {
+    super({
+      vertexShader: `
+      attribute vec2 uv;
+      attribute vec3 position;
+      attribute vec3 normal;
+
+      uniform mat4 projectionMatrix;
+      uniform mat4 modelViewMatrix;
+
+      varying vec2 vUv;
+
+      void main() {
+          gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.);
+          vUv = uv;
+        }
+    `,
+      fragmentShader: `
+      precision mediump float;
+      varying vec2 vUv;
+
+      void main() {
+        float strengthY = mod(vUv.y * 10.0, 1.);
+        strengthY = step(0.5, strengthY);
+
+        float strengthX = mod(vUv.x * 10.0, 1.);
+        strengthX = step(0.5, strengthX);
+
+        vec3 color = vec3(strengthY * strengthX);
+        gl_FragColor = vec4(color, 1.);
+      }
+      `,
+    });
+  }
+}
+
+export { FloorMaterial };
